02a1512907ca40b045d6690e1d96f0581b02bbdf
Ray <zai7lou@outlook.com>
2023-05-07 21:03:38 +0800

    update

--- scripts/install_template.cmd
+++ scripts/install_template.cmd
@@ -1,3 +1,12 @@
-echo "开始安装"
-dotnet new install ..\framework --force
+@echo off
+cd ..\framework
+
+echo "Cleaning"
+for /d /r %%c in (obj) do @if exist "%%c" (@rd /s /q "%%c" & echo Delete %%c)
+for /d /r %%c in (bin) do @if exist "%%c" (@rd /s /q "%%c" & echo Delete %%c)
+for /d /r %%c in (.vs) do @if exist "%%c" (@rd /s /q "%%c" & echo Delete %%c)
+echo Deletion complete.
+
+echo "Installing"
+dotnet new install .\ --force
 pause
--- scripts/install_template.sh
+++ scripts/install_template.sh
@@ -1,2 +1,13 @@
-echo "开始安装"
-dotnet new install ../framework --force
+#!/bin/bash
+
+cd ../framework
+
+echo "Cleaning"
+find . -type d -name "obj" -exec sh -c 'rm -rf "{}" && echo Delete {}' \;
+find . -type d -name "bin" -exec sh -c 'rm -rf "{}" && echo Delete {}' \;
+find . -type d -name ".vs" -exec sh -c 'rm -rf "{}" && echo Delete {}' \;
+echo "Deletion complete."
+
+echo "Installing"
+dotnet new install ./ --force
+read -p "Press [Enter] key to continue..."
